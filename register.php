7ac89057b9bd9df2eb444fa9cca641a2f5ae9d99
maxim <aidmax1234@gmail.com>
2025-01-21 12:49:46 +0300

    123

---
+++ register.php
@@ -0,0 +1,25 @@
+<?php
+require_once('db.php');
+$login = $_POST['login'];
+$pass = $_POST['pass'];
+$repeatpass = $_POST['repeatpass'];
+$email = $_POST['email'];
+ 
+
+if (empty($login) || empty($pass) || empty($repeatpass) || empty($email)) {
+    echo "заполните все поля";
+} 
+else {
+    if ($pass != $repeatpass){
+        echo "Пароли не совпадают";
+    } 
+    else {
+        $sql = "INSERT INTO `users` (login,pass,email) VALUES ('$login', '$pass', '$email')";
+        if ($conn -> query($sql) === TRUE){
+            echo "Успешная регестрация " . $login;
+        } 
+        else {
+            echo "Ошибка " . $conn->error;
+        }  
+    }
+}
